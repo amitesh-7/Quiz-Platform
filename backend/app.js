42a0ebf469898e4485413439b0d53f264e02e244
Amitesh Vishwakarma <amiteshvishwakarma2006@gmail.com>
2025-12-31 02:25:38 +0530

    minor updates

--- backend/app.js
+++ backend/app.js
@@ -65,8 +65,8 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
 // MongoDB connection middleware for Vercel serverless
 app.use(async (req, res, next) => {
